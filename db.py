1081c36ccac0a0b68efd1113d3c471f1eff8f213
Ivane Urjumelashvili <vanourjumelashvili@gmail.com>
2024-09-19 12:15:18 +0400

    db.py

--- db.py
+++ db.py
@@ -4,34 +4,33 @@ import random
 
 
 fake = Faker()
-con  = sqlite3.connect("Library.db")
 
+
+con = sqlite3.connect("Library.db")
 cursor = con.cursor()
 
-
-query = """
+cursor.execute("""
 CREATE TABLE IF NOT EXISTS book(
-        id INTEGER NOT NULL PRIMARY KEY,
-        name TEXT NOT NULL,
-        category TEXT NOT NULL,
-        page_number INTEGER NOT NULL,
-        date_issue DATE NOT NULL,
-        author_id INTEGER NOT NULL
-) 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,  
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    page_number INTEGER NOT NULL,
+    date_issue DATE NOT NULL,
+    author_id INTEGER NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES author(id)
+)
+""")
 
-"""
-cursor.execute(query)
 
-query="""
+cursor.execute("""
 CREATE TABLE IF NOT EXISTS author(
-        id INTEGER NOT NULL PRIMARY KEY,
-        name TEXT NOT NULL,
-        surname TEXT NOT NULL,
-        birth_date DATE NOT NULL,
-        birth_place TEXT NOT NULL
-        )
-"""
-cursor.execute(query)
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT NOT NULL,
+    surname TEXT NOT NULL,
+    birth_date DATE NOT NULL,
+    birth_place TEXT NOT NULL
+)
+""")
 
 
 def is_table_empty(table_name):
@@ -39,70 +38,69 @@ def is_table_empty(table_name):
     row_count = cursor.fetchone()[0]
     return row_count == 0
 
+
 def author_generator():
     authors = []
-    for i in range(1,501):
-        author = []
-        author.append(i)
-        author.append(fake.first_name())
-        author.append(fake.last_name())
-        author.append(fake.date_between(start_date='-65y', end_date='-25y'))
-        author.append(fake.city())
+    for _ in range(500):
+        author = (
+            fake.first_name(),
+            fake.last_name(),
+            fake.date_between(start_date='-65y', end_date='-25y'),
+            fake.city()
+        )
         authors.append(author)
     return authors
 
+
 if is_table_empty("author"):
     cursor.executemany("""
-    INSERT INTO author (id,name,surname,birth_date,birth_place) VALUES (?,?,?,?,?) 
-    """,author_generator())
-
+    INSERT INTO author (name, surname, birth_date, birth_place) VALUES (?,?,?,?)
+    """, author_generator())
 
 
 def book_generator():
     books = []
-    for i in range(1000):
-        book = []
-        book.append(i)
-        book.append(fake.sentence(nb_words=3))
-        book.append(fake.word())
-        book.append(random.randint(100,1000))
-        book.append(fake.date_between(start_date='-24y', end_date='now'))
-        book.append(random.randint(1,500))
+    for _ in range(1000):
+        book = (
+            fake.sentence(nb_words=3),
+            fake.word(),
+            random.randint(100, 1000),
+            fake.date_between(start_date='-24y', end_date='now'),
+            random.randint(1, 500)
+        )
         books.append(book)
     return books
 
+
 if is_table_empty("book"):
     cursor.executemany("""
-    INSERT INTO book (id,name,category,page_number,date_issue,author_id) VALUES (?,?,?,?,?,?)
-    """,book_generator())
+    INSERT INTO book (name, category, page_number, date_issue, author_id) VALUES (?,?,?,?,?)
+    """, book_generator())
 
 
-
-cursor.execute("SELECT * FROM book WHERE page_number = (SELECT max(book.page_number) FROM book)")
+cursor.execute("SELECT * FROM book WHERE page_number = (SELECT MAX(page_number) FROM book)")
 result = cursor.fetchall()
 print("Max page book:")
 for row in result:
     print(row)
 
-
-cursor.execute("SELECT avg(book.page_number) FROM book")
+cursor.execute("SELECT AVG(page_number) FROM book")
 print("\nAverage page number:")
 print(cursor.fetchone()[0])
 
 
-cursor.execute("SELECT max(author.birth_date) ,author.name FROM author")
+cursor.execute("SELECT MAX(birth_date), name FROM author")
 print("\nYoungest author:")
 print(cursor.fetchone())
 
 
 cursor.execute("""
-SELECT author.id, author.name
+SELECT id, name
 FROM author
 WHERE NOT EXISTS (
-    SELECT 1
-    FROM book
-    WHERE book.author_id = author.id
-)""")
+    SELECT 1 FROM book WHERE book.author_id = author.id
+)
+""")
 result = cursor.fetchall()
 print("\nAuthors without books:")
 for row in result:
@@ -124,5 +122,4 @@ for row in result:
 
 
 con.commit()
-
 con.close()
